aa7c7f7a96337973f73a0ee8b7b9487159869095
Dbitler <dbitler@hawk.iit.edu>
2023-01-20 13:50:07 -0800

    Initial Commit

---
+++ Quadratic Project/Quadratic_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quadratic_ProjectApp.swift
+//  Quadratic Project
+//
+//  Created by IIT PHYS 440 on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Quadratic_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
